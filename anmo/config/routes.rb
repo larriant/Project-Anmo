b7ab47811d6c247d24655f86b98be36f7b240812
Robbie <robbie.daniels.0@gmail.com>
2015-06-09 11:32:35 +0100

    Full user authentication + unique usernames added

--- anmo/config/routes.rb
+++ anmo/config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#index'
+
+  get 'sessions/new'
+
   #  Link the domain (input by the user) to an appropriate controller:
   resources :users
 
   #  Define a path for signup:
   get 'signup' => 'users#new'
+
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
