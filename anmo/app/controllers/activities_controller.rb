d79d41266869f2bea90c7026ee45c38cfaad97a6
Robbie <robbie.daniels.0@gmail.com>
2015-06-10 00:24:20 +0100

    Fixed activity incorrect error message and none-user create activity bugs, checked code

--- anmo/app/controllers/activities_controller.rb
+++ anmo/app/controllers/activities_controller.rb
@@ -1,20 +1,13 @@
 class ActivitiesController < ApplicationController
+
+  before_action :logged_in_user
+
   def show
     @activity = Activity.find(params[:id])
   end
 
-  def destroy
-    Activity.find(params[:id]).destroy
-    flash[:success] = "Activity deleted"
-    redirect_to activities_url
-  end
-  
   def new
     @activity = current_user.activities.build
-  end
-
-  def index
-    @activities = current_user.activities.all
   end
 
   def create
@@ -28,8 +21,8 @@ class ActivitiesController < ApplicationController
     end
   end
 
-  def edit
-    @activity = Activity.find(params[:id])
+  def index
+    @activities = current_user.activities.all
   end
 
   def update
@@ -42,10 +35,27 @@ class ActivitiesController < ApplicationController
     end
   end
 
+  def edit
+    @activity = Activity.find(params[:id])
+  end
+
+  def destroy
+    Activity.find(params[:id]).destroy
+    flash[:success] = "Activity deleted"
+    redirect_to activities_url
+  end
+  
   private
 
   def activity_params
     params.require(:activity). permit(:activity_name, :importance, :frequency, :activity_type, :start_time, :end_time, :completion_date, :user_id)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "You must be a user to edit activities."
+      redirect_to root_url
+    end
   end
 
 end
--- anmo/app/models/activity.rb
+++ anmo/app/models/activity.rb
@@ -1,5 +1,8 @@
 class Activity < ActiveRecord::Base
+
   belongs_to :user
+
   validates :user_id, presence: true
+  validates :event_name, presence: true
 
 end
