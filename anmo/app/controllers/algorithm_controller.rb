990a1639bf8303bcefecfc3d6fdd8f37ebf0db9e
Julyan Davey <me@julyandavey.com>
2015-06-10 16:03:16 +0100

    First version events_ouput created

--- anmo/app/controllers/algorithm_controller.rb
+++ anmo/app/controllers/algorithm_controller.rb
@@ -2,7 +2,23 @@ class AlgorithmController < ApplicationController
   before_action :logged_in_user
 
   def index
-  @time = Time.new(2015)
-  @events = current_user.events.all
+    @events = current_user.events.all
+    @now = Time.now.change(hour: 0, min: 0, sec:0)
+  
+    @events_output = {} 
+    (9..18).step(1) do |n|
+      @events.each do |e|
+        if e.start_time.hour == n
+          @events_output[@now.change(hour: n)] = e
+          break 
+        elsif e.end_time.hour == n
+          @events_output[@now.change(hour: n-1)] = e
+          next 
+        else
+          @events_output[@now.change(hour: n)] = ""
+        end
+      end
+    end
+  
   end
 end
